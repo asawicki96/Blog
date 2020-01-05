59fd66dfe66260db31d299689a1ca47a43ed54d7
Adam Sawicki <asawicki1996@gmail.com>
2020-01-05 15:57:26 +0100

    Simple blog chapter 1

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 # Create your views here.
 
-def post_list(request):
-    posts = Post.published.all()
+'''def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html', 
-                  {'posts':posts})
+                  {'page':page,
+                   'posts':posts})
 
+'''
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post,
                                    status = 'published',
@@ -17,3 +29,9 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
