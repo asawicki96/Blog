74f099e42358ac55851cc7a2352c9a1900e769b7
Adam Sawicki <asawicki1996@gmail.com>
2020-01-13 20:09:29 +0100

    Features: tags, comments, sharing

--- MyBlog/blog/urls.py
+++ MyBlog/blog/urls.py
@@ -3,12 +3,13 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('list/', views.PostListView.as_view(), name = 'post_list'),
+    re_path('list/', views.post_list, name = 'post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/'\
             r'(?P<post>[-\w]+)/',
             views.post_detail,
             name = 'post_detail'),
     re_path(r'^(?P<post_id>\d+)/share/$', views.post_share,
             name='post_share'),
+    re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name = 'post_list_by_tag'),
 ]
 
--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -4,10 +4,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
-'''def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -19,9 +27,10 @@ from django.core.mail import send_mail
     return render(request,
                   'blog/post/list.html', 
                   {'page':page,
-                   'posts':posts})
+                   'posts':posts,
+                   'tag': tag})
 
-'''
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post,
                                    status = 'published',
@@ -40,12 +49,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
         new_comment = None
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                  .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'comment_form': comment_form,
-                   'new_comment': new_comment})
+                   'new_comment': new_comment,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id= post_id, status='published')
@@ -77,3 +94,4 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+    
